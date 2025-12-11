dd72fdbfd28e4724b275435895a4f1d00b7c65c1
Tomasz Łabędzki <tlabedzki00@gmail.com>
2025-12-11 19:40:39 +0100

    commid

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const cells = [];
+  for (let i = 0; i < 20; i++) {
+    cells.push(new Array());
+    for (let j = 0; j < 20; j++) {
+      cells[i][j] = <div data-x={i} data-y={j}></div>;
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='gird'>
+      {cells}
+    </div>
   )
 }
 
